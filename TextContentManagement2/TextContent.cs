b4b360aaae7b70ee29452ea36e1f5526b321e827
askfrmtwo <askfrm2@proton.me>
2025-05-19 14:26:55 +0000

    Add files via upload
    1. Files uploaded after the folder creation (manually, without using .gitignore). 
    
    To use .gitignore (via github.com website):
    a) Create .gitignore inside needed repository;
    b) Edit the file and include files in the format:    /bin
                                                                            /obj
                                                                            /explanation.txt
                                                                            and so on...
    c) If using web version - choose "Commit changes", if using local Git, run:   git add .gitignore
                                                                                                                       git commit -m "Add .gitignore to exclude bin and obj folders, and explanation.txt file"
                                                                                                                       git push
    
    2. A lite version for TextContentManagement2 (or Library Management) redone for simplicity (with possibility to add complex methods for verifying TextContent parameters input, etc.).

---
+++ TextContentManagement2/TextContent.cs
@@ -0,0 +1,18 @@
+﻿namespace TextContentManagement2
+{
+    public class TextContent // Creating a "class" that is "public" - a defenition for a computer for what to work with that can be accesses by other classes.
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string SerialNumber { get; set; }
+        public int Year { get; set; }
+
+        public TextContent(string title, string author, string serialNumber, int year) // Creating a constructor for reventing null references to the parameters of the class. 
+        {
+            Title = title; // No verification of parameters for simplicity.
+            Author = author;
+            SerialNumber = serialNumber;
+            Year = year;
+        }
+    }
+}
